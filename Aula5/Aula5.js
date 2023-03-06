21eafd3e9954939641ef0e4ceb7f81f38bd5f438
kaue <kaueottocash@gmail.com>
2023-03-06 14:55:50 -0300

    Operadores Logicos

---
+++ Aula5/Aula5.js
@@ -0,0 +1,19 @@
+/* Operadores lógicos
+
+>
+<
+>=
+<=
+==
+!=
+
+*/
+
+var num1=10,num2=5
+
+console.log(num1 > num2)
+console.log(num1 < num2)
+console.log(num1 >= num2)
+console.log(num1 <= num2)
+console.log(num1 == num2)
+console.log(num1 != num2)
