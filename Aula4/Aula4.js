68854b9b7787dcf4ae78ef13338df406b9806a5e
kaue <kaueottocash@gmail.com>
2023-03-06 14:51:54 -0300

    Operadores Logicos

---
+++ Aula4/Aula4.js
@@ -0,0 +1,19 @@
+/*
+Operadores lógicos
++
+-
+*
+/
+%
++=
+-=
+*/
+
+let num1=0,num2=0;
+num1 = 2
+num2 = 11
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num2/num1)
+console.log(num2%num1)
